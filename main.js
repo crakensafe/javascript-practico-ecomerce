9214362aad71f3c510090894a102693a0fc6eca0
crakensafe <crakensafe@icloud.com>
2024-03-06 21:02:59 -0600

    cracion de carrito de compras e interactividad entre menu de correo y menu de categorias

--- main.js
+++ main.js
@@ -2,14 +2,53 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+
+
 
 menuEmail.addEventListener("click", toogleDesktopMenu);
-menuHamIcon.addEventListener("click", toogleMobileMenu)
+menuHamIcon.addEventListener("click", toogleMobileMenu);
+menuCarritoIcon.addEventListener("click", toogleCarritoAside);
+
+
+
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+
+    const isAsideClose = aside.classList.contains("inactive")
+
+    if (!isAsideClose) {
+        aside.classList.add("inactive");
+    }
 }
 
+
 function toogleMobileMenu(){
+
+    const isAsideClose = aside.classList.contains("inactive")
+
+    if (!isAsideClose) {
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive")
+
+}
+
+function toogleCarritoAside(){
+    const isMenuMobileClose = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClose = menuEmail.classList.contains("inactive");
+
+    if (!isMenuMobileClose) {
+        mobileMenu.classList.add("inactive");
+    } 
+    if (!isDesktopMenuClose) {
+        desktopMenu.classList.add("inactive")
+    }
+    
+
+    aside.classList.toggle("inactive")
 }
